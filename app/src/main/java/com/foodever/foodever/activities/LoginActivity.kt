c12cd765d514af8960178d0b6cbdc96ab57beb29
Jomel Macion <macion.jomel04@gmail.com>
2018-11-12 00:04:51 +0800

    Updated with Login Activity

--- app/src/main/java/com/foodever/foodever/activities/LoginActivity.kt
+++ app/src/main/java/com/foodever/foodever/activities/LoginActivity.kt
@@ -2,12 +2,36 @@ package com.foodever.foodever.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.foodever.foodever.R
+import es.dmoral.toasty.Toasty
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    val tag: String = "LoginActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        buttonLogin.setOnClickListener {
+            val username: String = editTextUsername.text.toString()
+            val password: String = editTextPassword.text.toString()
+            Toasty.info(this, "Username: $username\nPassword: $password", Toast.LENGTH_LONG, true).show()
+        }
+
+        buttonFacebook.setOnClickListener {
+            Toasty.info(this, "Sign in with Facebook!", Toast.LENGTH_LONG, true).show()
+        }
+
+        buttonGoogle.setOnClickListener {
+            Toasty.info(this, "Sign in with Google!", Toast.LENGTH_LONG, true).show()
+        }
+
+        textViewCreateAccount.setOnClickListener {
+            Toasty.info(this, "Create an account!", Toast.LENGTH_LONG, true).show()
+        }
     }
 }
